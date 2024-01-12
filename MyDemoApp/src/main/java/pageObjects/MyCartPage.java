58cc848b657be811425c575bbab04a93c1d84d6f
Javier Meléndez <jmz.krarid@outlook.com>
2024-01-12 12:00:58 -0600

    In case the user is already logged in, doesn't logged in again

--- MyDemoApp/src/main/java/pageObjects/MyCartPage.java
+++ MyDemoApp/src/main/java/pageObjects/MyCartPage.java
@@ -1,8 +1,16 @@
 package pageObjects;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
@@ -21,9 +29,31 @@ public class MyCartPage extends AndroidActions {
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
 	
-	public LoginPage proceedToCheckout()
+	public ShipmentPage proceedToCheckout() throws IOException
 	{
+		Properties property = new Properties();
+		FileInputStream propertyFile = new FileInputStream(System.getProperty("user.dir") + "\\src\\main\\java\\resources\\data.properties");
+		
+		property.load(propertyFile);
+		
 		proceedButton.click();
-		return new LoginPage(driver);
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+		
+		boolean loggedIn = false;
+		
+		try {
+			wait.until(ExpectedConditions.textToBePresentInElementLocated(AppiumBy.xpath("//*[@content-desc='container header']/android.widget.TextView"), "Login"));
+		} catch(Exception e) {
+			loggedIn = true;
+		}
+		
+		if(!loggedIn)
+		{
+			LoginPage login = new LoginPage(driver);
+			login.login(property.getProperty("username"), property.getProperty("password"));
+		}
+		
+		return new ShipmentPage(driver);
 	}
 }
