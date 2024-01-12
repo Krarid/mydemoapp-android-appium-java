37fc3b59233472995f4eba4335bf79468d18aebc
Javier Meléndez <jmz.krarid@outlook.com>
2024-01-12 11:57:33 -0600

    Add Data Provider to MDA-1 test case
    data.json to get the data from a json

--- MyDemoApp/src/test/java/tests/Catalog.java
+++ MyDemoApp/src/test/java/tests/Catalog.java
@@ -1,9 +1,15 @@
 package tests;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
 import org.testng.Assert;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import pageObjects.LoginPage;
+import pageObjects.CompletePage;
 import pageObjects.MyCartPage;
 import pageObjects.PaymentPage;
 import pageObjects.ProductPage;
@@ -13,77 +19,43 @@ import utils.BaseTest;
 
 public class Catalog extends BaseTest {
 	
-	private String productName;
-	private String color;
-	private int quantity;
-	
-	private String username;
-	private String password;
-	private String fullName;
-	private String address;
-	private String city;
-	private String state;
-	private String zipCode;
-	
-	private String country;
-	private String cardNumber;
-	private String expiration;
-	private String securityCode;
-	
-	public Catalog()
+	@Test(dataProvider="getData", groups= {"Smoke"})
+	public void BuyProduct(HashMap<String, String> input) throws InterruptedException, IOException
 	{
-		productName = "Onesie";
-		color = "black";
-		quantity = 3;
-		
-		username = "bob@example.com";
-		password = "10203040";
-		fullName = "Rebecca Winter";
-		address = "Mandorley 112";
-		city = "Truro";
-		state = "Cornwall";
-		zipCode = "89750";
-		
-		country = "United Kingdom";
-		cardNumber = "325812657568789";
-		expiration = "0325";
-		securityCode = "123";
-	}
-	
-	@Test
-	public void BuyProduct() throws InterruptedException
-	{
-		ProductPage product = products.chooseProduct(productName);
-		product.chooseColor(color);
-		product.chooseQuantity(quantity);
+		ProductPage product = products.chooseProduct(input.get("productName"));
+		product.chooseColor(input.get("color"));
+		product.chooseQuantity(Integer.parseInt(input.get("quantity")));
 		product.addToCart();
 		MyCartPage cart = product.goToCart();
 		
-		LoginPage login = cart.proceedToCheckout();
-		login.enterUsername(username);
-		login.enterPassword(password);
-		
-		ShipmentPage shipment = login.login();
-		shipment.enterFullName(fullName);
-		shipment.enterAddress(address);
-		shipment.enterCity(city);
-		shipment.enterState(state);
-		shipment.enterZipCode(zipCode);
-		shipment.enterCountry(country);
+		ShipmentPage shipment = cart.proceedToCheckout();
+		shipment.enterFullName(input.get("fullName"));
+		shipment.enterAddress(input.get("address"));
+		shipment.enterCity(input.get("city"));
+		shipment.enterState(input.get("state"));
+		shipment.enterZipCode(input.get("zipCode"));
+		shipment.enterCountry(input.get("country"));
 		
 		PaymentPage payment = shipment.toPayment();
 		Thread.sleep(500);
-		payment.enterFullName(fullName);
-		payment.enterCardNumber(cardNumber);
-		payment.enterExpiration(expiration);
-		payment.enterSecurityCode(securityCode);
+		payment.enterFullName(input.get("fullName"));
+		payment.enterCardNumber(input.get("cardNumber"));
+		payment.enterExpiration(input.get("expiration"));
+		payment.enterSecurityCode(input.get("securityCode"));
 		
 		ReviewPage review = payment.reviewOrder();
-		Assert.assertTrue( review.getProduct().contains(productName) );
-		Assert.assertTrue( review.getColor().contains(color) );
+		Assert.assertTrue( review.getProduct().contains(input.get("productName")) );
+		Assert.assertTrue( review.getColor().contains(input.get("color")) );
 		
-		review.placeOrder();
+		CompletePage complete = review.placeOrder();
+		complete.continueShopping();
+	}
+	
+	@DataProvider
+	public Object[][] getData() throws IOException
+	{
+		List<HashMap<String, String>> data = getJsonData("C:\\Users\\javie\\OneDrive\\Documents\\Github\\mydemoapp-android-appium-java\\MyDemoApp\\src\\test\\java\\data\\data.json");
 		
-		Thread.sleep(1500);
+		return new Object[][] { {data.get(0)}, {data.get(1)} };
 	}
 }
