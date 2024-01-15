4f2ec136464b369999515157d8d231047b71f0a4
Javier Meléndez <jmz.krarid@outlook.com>
2024-01-15 14:34:47 -0600

    MDA-3: Increase the cart

--- MyDemoApp/src/test/java/tests/Catalog.java
+++ MyDemoApp/src/test/java/tests/Catalog.java
@@ -1,6 +1,8 @@
 package tests;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.text.DecimalFormat;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
@@ -64,17 +66,69 @@ public class Catalog extends BaseTest {
 		product.chooseColor(input.get("color"));
 		product.chooseQuantity(Integer.parseInt(input.get("quantity")));
 		product.addToCart();
+		
 		MyCartPage cart = product.goToCart();
 		cart.removeItem();
 		wait.until(ExpectedConditions.textToBePresentInElementLocated(AppiumBy.xpath("//*[@content-desc='container header']/android.widget.TextView"), "No Items"));
+		
 		Assert.assertEquals(cart.getNoItemsText(), "No Items");
 		cart.goShopping();
 	}
 	
-	@DataProvider
-	public Object[][] getData() throws IOException
+	@Test(dataProvider="getData")
+	public void IncreaseTheCart(HashMap<String, String> input) throws InterruptedException, IOException
 	{
-		List<HashMap<String, String>> data = getJsonData("C:\\Users\\javie\\OneDrive\\Documents\\Github\\mydemoapp-android-appium-java\\MyDemoApp\\src\\test\\java\\data\\data.json");
+		int items = Integer.parseInt( input.get("quantity") );
+		float price, delivery = 5.99f;
+		
+		final DecimalFormat df = new DecimalFormat("0.00");
+		
+		ProductPage product = products.chooseProduct(input.get("productName"));
+		product.addToCart();
+		price = product.getPrice();
+		
+		MyCartPage cart = product.goToCart();
+		cart.increment(items);
+		
+		Thread.sleep(500);
+		
+		Assert.assertEquals(cart.getTotalPrice(), "$" + df.format(price * items) );
+		
+		ShipmentPage shipment = cart.proceedToCheckout();
+		shipment.enterShipmentDetails();
+		
+		PaymentPage payment = shipment.toPayment();
+		Thread.sleep(500);
+		payment.enterPaymentDetails();
+		
+		ReviewPage review = payment.reviewOrder();
+		Assert.assertEquals(review.getTotalPrice(), "$" + df.format(price * items + delivery) );
+		
+		CompletePage complete = review.placeOrder();
+		complete.continueShopping();
+	}
+	
+	@DataProvider
+	public Object[][] getData(Method m) throws IOException
+	{
+		String dataFile = "1";
+		
+		switch( m.getName() ) {
+		
+			case "BuyProduct":
+				dataFile = "1";
+			break;
+		
+			case "RemoveProduct":
+				dataFile = "2";
+			break;
+			
+			case "IncreaseTheCart":
+				dataFile = "3";
+			break;
+		}
+		
+		List<HashMap<String, String>> data = getJsonData("C:\\Users\\javie\\OneDrive\\Documents\\Github\\mydemoapp-android-appium-java\\MyDemoApp\\src\\test\\java\\data\\mda" + dataFile + ".json");
 		
 		return new Object[][] { {data.get(0)}, {data.get(1)} };
 	}
