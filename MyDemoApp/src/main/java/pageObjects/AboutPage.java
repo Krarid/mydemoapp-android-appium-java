8064010838bc065bc5b2e17857f6fe58e1217bad
Javier Meléndez <jmz.krarid@outlook.com>
2024-02-19 12:41:04 -0600

    Fix: Wait until SauceLabsWebsite is opened
    MDA-14: Go to the sauce labs website external link test case

--- MyDemoApp/src/main/java/pageObjects/AboutPage.java
+++ MyDemoApp/src/main/java/pageObjects/AboutPage.java
@@ -1,7 +1,11 @@
 package pageObjects;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
@@ -31,6 +35,10 @@ public class AboutPage extends AndroidActions {
 	
 	public void goToSauceLabs()
 	{
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
 		sauceLabsWebsite.click();
+		
+		wait.until(ExpectedConditions.invisibilityOf(sauceLabsWebsite));
 	}
 }
